96599a036b62931dcd24e309bbde343a5f0f6a25
jamiefoxx070 <92607120+jamiefoxx070@users.noreply.github.com>
2021-10-15 19:55:30 -0300

    Update ssiptvarg.php

--- ssiptvarg.php
+++ ssiptvarg.php
@@ -1,4 +1,4 @@
-<?php 
-     readfile('https://raw.githubusercontent.com/mortal251/ssiptvarg/main/fase2');     
-     exit();
-?>
+$handler = curl_init("https://raw.githubusercontent.com/mortal251/ssiptvarg/main/fase2");  
+   $response = curl_exec ($handler);  
+   curl_close($handler); 
+   echo $response;
