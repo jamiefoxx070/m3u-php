377f74ea9bbd23468c98f9746644300fdb7f2c0e
jamiefoxx070 <92607120+jamiefoxx070@users.noreply.github.com>
2021-10-15 19:34:27 -0300

    Update ssiptvarg.php

--- ssiptvarg.php
+++ ssiptvarg.php
@@ -1,8 +1,4 @@
-<html>
- <head>
-  <title>PHP-Test</title>
- </head>
- <body>
-  <?php header ("Location: https://raw.githubusercontent.com/mortal251/ssiptvarg/main/fase2.m3u");?>
- </body>
-</html>
+<?php 
+     header('Location: https://raw.githubusercontent.com/mortal251/ssiptvarg/main/fase2.m3u');
+     exit();
+?>
